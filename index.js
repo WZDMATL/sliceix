a4640c508dbbe5f789b8fd145cb586591c90e846
WZDMATL <wzdm315@gmail.com>
2022-03-09 23:28:25 -0500

    Made input elements required for the form

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 function clickDiscord() {
-            let username = document.querySelector("span.h1-email");
-            username.innerHTML = "What is your Discord username?";
+  let username = document.querySelector("span.h1-email");
+  username.innerHTML = "What is your Discord username?";
 
-            let bDiscord = document.querySelector("p");
-            bDiscord.innerHTML = `<br />
+  let bDiscord = document.querySelector("p");
+  bDiscord.innerHTML = `<br />
                 <div class="text-center">
-                <form>
+                <form class="needs-validation">
                     <div class="mb-3">
                         <label for="InputUsername1" class="form-label">Username</label>
-                        <input class="text-center" type="text" id="form-control" id="InputUsername1" aria-describedby="usernamelHelp">
+                        <input class="text-center" type="text" id="form-control" id="InputUsername1" aria-describedby="usernamelHelp" required>
                         <div id="usernameHelp" class="form-text">We'll never share your information with anyone else.</div>
                     </div>
                     <div class="mb-3 form-check">
-                        <input class="text-center" type="checkbox" id="form-check-input" id="Check1">
-                        <label class="form-check-label" for="Check1">Check me out</label>
+                        <input class="text-center" type="checkbox" id="invalidCheck" required>
+                        <label class="form-check-label" for="invalidCheck">Accept <a class="pp" alt="Privacy Policy" type="button" data-bs-toggle="modal" data-bs-target="#ppModal">Privacy Policy and Terms of Use</a></label>
                     </div>
+                    <div>
                     <button type="submit" class="btn">Enter Discord</button>
+                    </div>
                 </form>
                 </div>
                 <br />`;
@@ -25,5 +27,5 @@ let buttonEmail = document.querySelector(".email-button");
 buttonEmail.addEventListener("click", clickDiscord);
 
 function relocate_id() {
-    location.href = "#h1-text";
+  location.href = "#h1-text";
 }
